97a4a3e34c82706f2ff11380bdeee71b2d363277
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-08-27 16:50:11 -0300

    livro algoritmos atualizado

---
+++ faculdade/Livro Algoritmos/exercicios em python/capitulo7/grafoDijkstra.py
@@ -0,0 +1,16 @@
+grafo = {}
+grafo["inicio"] = {}
+
+grafo["inicio"]["a"] = 6
+grafo["inicio"]["b"] = 2
+
+grafo["a"] = {}
+grafo["a"]["fim"] = 1
+
+grafo["b"] = {}
+grafo["b"]["a"] = 3
+grafo["b"]["fim"] = 5
+
+grafo["fim"] = {}
+
+print(grafo)
