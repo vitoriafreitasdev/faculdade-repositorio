0121c35e5238702814af8214353ea54d5c0ea8fc
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-09-05 18:02:56 -0300

    ok

--- faculdade/segundo_semestre/estruturadedadosempython/pratica/teste.py
+++ faculdade/segundo_semestre/estruturadedadosempython/pratica/teste.py
@@ -1,17 +1,31 @@
-def pesquisa_binaria(lista, item):
-    baixo = 0
-    alto = len(lista) - 1
-    while baixo <= alto:
-        meio = (baixo + alto) // 2
-        chute = lista[meio]
-        if chute == item:
-            return meio
-        if chute > item:
-            alto = meio - 1
-        else:
-            baixo = meio + 1
-    return None
-minha_lista = [1, 3, 5, 7, 9]
 
-print(pesquisa_binaria(minha_lista, 9))
-print(pesquisa_binaria(minha_lista, -1))
+# class No:
+#     def __init__(self, dado, esq=None, dir=None):
+#         self.dado = dado
+#         self.esq = esq 
+#         self.dir = dir 
+    
+# def em_ordem(raiz, visita=None):
+#     if visita is None:
+#         visita = []
+#     if raiz:
+#         em_ordem(raiz.esq, visita)
+#         visita.append(raiz.dado)
+#         em_ordem(raiz.dir, visita)
+#     return visita
+
+
+# no_a = No("A")
+# no_j = No("J")
+# no_e = No("E", no_a, no_j)
+# no_r = No("R")
+# no_y = No("Y", no_r)
+# raiz = No("M", no_e, no_y)
+
+# print(" ".join(em_ordem(raiz)))
+
+lista = [2, 4, 6, 8]
+n = len(lista)
+
+mediana = lista[n//2] if n % 2 else (lista[n//2-1] + lista[n//2])/2
+print(mediana)
