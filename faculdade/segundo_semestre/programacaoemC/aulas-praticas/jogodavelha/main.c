b02b1b1d2838105ed9fadfdb7327956a8b634d0c
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-09-16 15:00:56 -0300

    ok

--- faculdade/segundo_semestre/programacaoemC/aulas-praticas/jogodavelha/main.c
+++ faculdade/segundo_semestre/programacaoemC/aulas-praticas/jogodavelha/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 int main()
 {
     char mat[3][3] =
@@ -18,8 +18,49 @@ int main()
         printf("\n");
     }
 
+    printf("\n");
 
     mat[0][2] = 'X';
+    mat[1][2] = 'X';
+    mat[2][2] = 'X';
+
+    bool fim = false;
+
+    // diagonal 1
+    if (mat[0][0] == 'X' && mat[1][1] == 'X' && mat[2][2] == 'X'){
+        fim = true;
+    }
+    // diagonal 2
+    if (mat[0][2] == 'X' && mat[1][1] == 'X' && mat[2][0] == 'X'){
+        fim = true;
+    }
+
+    // linha 1
+    if (mat[0][0] == 'X' && mat[0][1] == 'X' && mat[0][2] == 'X'){
+        fim = true;
+    }
+    // linha 2
+    if (mat[1][0] == 'X' && mat[1][1] == 'X' && mat[1][2] == 'X'){
+        fim = true;
+    }
+    // linha 3
+    if (mat[2][0] == 'X' && mat[2][1] == 'X' && mat[2][2] == 'X'){
+        fim = true;
+    }
+
+
+    // coluna 1
+    if (mat[0][0] == 'X' && mat[1][0] == 'X' && mat[2][0] == 'X'){
+        fim = true;
+    }
+    // coluna 2
+    if (mat[0][1] == 'X' && mat[1][1] == 'X' && mat[2][1] == 'X'){
+        fim = true;
+    }
+    // coluna 3
+    if (mat[0][2] == 'X' && mat[1][2] == 'X' && mat[2][2] == 'X'){
+        fim = true;
+    }
 
     for (i = 0; i < 3; i++){
         for (j = 0; j < 3; j++){
@@ -27,6 +68,8 @@ int main()
         }
         printf("\n");
     }
-
+    if(fim){
+        printf("Jogo acabou");
+    }
     return 0;
 }
