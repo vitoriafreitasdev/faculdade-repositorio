e6137c91d5f7eac322722cbf12fe780acb5b881f
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-10-23 18:23:07 -0300

    ok

--- faculdade/segundo_semestre/programacaoemC/aulas-praticas/aula4/main.c
+++ faculdade/segundo_semestre/programacaoemC/aulas-praticas/aula4/main.c
@@ -6,70 +6,12 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    int i = 0;
-
-    while(i <= 5){
-        printf("%d\n", i);
-        i++;
-    }
-    //
-
-    char opcao;
-
-    do {
-        printf("Deseja continuar? (s/n): ");
-        scanf(" %c", &opcao);
-    } while (opcao == 's' || opcao == 'S');
-
-    printf("Fim do programa.\n");
-
-    //
-
-    int I;
-    for (I = 1; I <= 5; I++){
-        printf("Valor: %d\n", I);
-    }
-
-
-    //
-
-    int o, j;
-
-    for(o = 1; o <= 10; o++){
-        printf("\nTabuada do %d:\n", o);
-
-        for(j = 1; j <= 10; j++){
-            printf("%d x %d = %d\n", o, j, o * j);
-        }
-
-    }
-    // desafio
-
-    float numero;
-    float soma;
-    float media;
-    int quantidade = 0;
-
-    while(numero != 0){
-        printf("Digite um numero: ");
-        scanf("%f", &numero);
-        if(numero > 0){
-            soma += numero;
-            quantidade++;
-        }
-    }
-
-    media = soma / quantidade;
-
-    printf("Media: %.2f\n", media);
-
-    // desafio
-
+    // Quantidade de linhas que "árvore" vai ter
     int linhas;
 
     printf("Quantidade de linhas: ");
     scanf("%d", &linhas);
-
+    // Árvore feita com um laço aninhado
     int l, k;
     for (l = 1; l <= linhas; l++){
 
@@ -82,4 +24,5 @@ int main()
 
 
     return 0;
+
 }
